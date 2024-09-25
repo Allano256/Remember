722bfe0d96abdd8daad6b753976193a58e02ecae
allan zizinga <allanzizinga@yahoo.com>
2024-09-26 01:55:58 +0200

    At implementing CitiesList

--- remember/src/components/CountriesList.jsx
+++ remember/src/components/CountriesList.jsx
@@ -1,11 +1,22 @@
-import styles from './CountriesList.module.css'
+import CityItem from './CityItem';
+import styles from './CityList.module.css'
+import Spinner from './Spinner'
+import Message from "./Message"
 
-function CountriesList() {
+function CountriesList({cities, isLoading}) {
+    // This function will return a list of all the cities visited.
+    
+
+    if(isLoading) return <Spinner />;
+
+     // Incase there is no cities, a message will be returned
+     if(!cities.length) return <Message message="Please click on the map to get started..." />
+
     return (
-        <ul>
-           List 
+        <ul className={styles.cityList} >
+           {cities.map((city)=> <CityItem city={city} key={city.id} /> )}
         </ul>
     )
 }
 
-export default CountriesList
+export default CityList
