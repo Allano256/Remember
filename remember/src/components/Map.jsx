dd79d18b4e7cc8cd96de0aa1b404e000a8e620de
allan zizinga <allanzizinga@yahoo.com>
2024-09-27 09:21:24 +0200

    Fixed error in dummy cities data

--- remember/src/components/Map.jsx
+++ remember/src/components/Map.jsx
@@ -12,10 +12,11 @@ function Map() {
     const [mapPosition, setMapPosition] = useState([40, 0])
      const [searchParams]= useSearchParams();
 
-     const mapLat = searchParams.get("lat")
+     const mapLat = searchParams.get("lat");
      const mapLng =searchParams.get("lng");
 
    useEffect(function(){
+   
     if(mapLat && mapLng) setMapPosition([mapLat, mapLng])
    },[mapLat, mapLng])
 
@@ -24,8 +25,6 @@ function Map() {
         
         <MapContainer 
         center={mapPosition}
-      
-       
          zoom={7} 
          scrollWheelZoom={true}
            className={styles.map} >
@@ -52,12 +51,14 @@ function Map() {
 }
 
 function SwitchCenter({position}){
+    // This custom hook will move to that specific position on the map
     const map = useMap();
     map.setView(position);
     return null;
 }
 
 function DetectClick(){
+    // This custom hook will detect any click on the map and immediately show the map
     
     const navigate = useNavigate()
     useMapEvents({
