4af1cf78a6032701338107d52f99341fc9c400af
Allan Zizinga <allanzizinga@yahoo.com>
2025-01-02 23:02:43 +0000

    Added key to date picker

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -132,9 +132,7 @@ function getCSRFToken(){
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
         
-
-        {/* <DatePicker onChange={(date)=> setDate(date)} selected={date} dateFormat="yyyy-MM-dd" id="date" /> */}
-        <DatePicker onChange={(date)=> setDate(date)} selected={date} dateFormat='dd/MM/yyyy' id="date" />
+        <DatePicker onChange={(date)=> setDate(date)} key={date} selected={date} dateFormat='dd/MM/yyyy' id="date" />
       </div>
 
       <div className={styles.row}>
