3b87d9477c5297c60cb2c224056792cc2f9ae717
Allan Zizinga <allanzizinga@yahoo.com>
2025-01-07 18:04:42 +0000

    Testing saved date,  fiexed typo error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -91,7 +91,7 @@ function getCSRFToken(){
 
  async function handleSubmit(e){
   e.preventDefault();
-  cpnsole.log(date)
+  console.log(date)
   if(!cityName || !date) return;
 
   const newCity= {
